6324b1f4a93630b8beff5fa5be1669121a67aee7
Vanaja Vanaja <vanajava@wm.ibm.com>
2024-12-07 12:15:41 +0800

    Add entity for Saving_detail and Account_Account_type

---
+++ src/main/java/com/squad1/hackathon/entity/AccountAccountType.java
@@ -0,0 +1,45 @@
+package com.squad1.hackathon.entity;
+
+import jakarta.persistence.*;
+
+
+
+@Entity
+@Table(name = "account_account_type")
+public class AccountAccountType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_id", nullable = false)
+    private Long accountId;
+
+    @Column(name = "account_type_id", nullable = false)
+    private Long accountTypeId;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public Long getAccountTypeId() {
+        return accountTypeId;
+    }
+
+    public void setAccountTypeId(Long accountTypeId) {
+        this.accountTypeId = accountTypeId;
+    }
+}
