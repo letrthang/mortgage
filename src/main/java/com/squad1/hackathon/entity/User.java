6359103779c9adb63298eb783fc34fdf12779850
Trung Thang Le <le_trung_thang@nsearchglobal.com>
2024-12-07 12:24:40 +0800

    entity

--- src/main/java/com/squad1/hackathon/entity/User.java
+++ src/main/java/com/squad1/hackathon/entity/User.java
@@ -6,12 +6,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
 
+import java.sql.Timestamp;
+
 @Entity
 @Table(name = "user")
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
-@ToString(onlyExplicitlyIncluded=true)
+@ToString(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
 public class User {
@@ -24,5 +26,10 @@ public class User {
     private String name;
     @Column(name = "password")
     private String password;
-
+    @Column(name = "dob")
+    private Timestamp dob;
+    @Column(name = "gender")
+    private String gender;
+    @Column(name = "role")
+    private String role;
 }
